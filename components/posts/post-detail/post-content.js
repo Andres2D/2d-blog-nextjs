bcdb0072fbb7f0dadb5623c79a6b1945651229bc
Andres2D <andres2d1997.1206@gmail.com>
2022-08-20 15:56:37 -0500

    Add post detal components and design

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,26 @@
+import PostHeader from './post-header';
+import styles from './post-content.module.css';
+
+const DUMMY_POST = {
+  title: 'Getting Started with NextJS',
+  slug: 'getting-started-with-next-js',
+  image: 'getting-started-with-next-js.png',
+  date: '2022-08-19',
+  content: '# This is a first post'
+};
+
+const PostContent = () => {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={styles.content}>
+      <PostHeader 
+        title={DUMMY_POST.title} 
+        image={imagePath}
+      />
+      {DUMMY_POST.content}
+    </article>
+  );
+};
+
+export default PostContent;
---
+++ components/posts/post-detail/post-header.js
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import styles from './post-header.module.css'
+
+const PostHeader = (props) => {
+  const { title, image } = props;
+  
+  return (
+    <header className={styles.header}> 
+      <h1 className={styles.title}>{title}</h1>
+      <Image 
+        className={styles.image}
+        src={image} 
+        alt={title} 
+        width={200} 
+        height={150} 
+      />
+    </header>
+  )
+};
+
+export default PostHeader;
