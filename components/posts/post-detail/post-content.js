adb8c28e0a1f7424bd9757c8efe5278729391312
Andres2D <andres2d1997.1206@gmail.com>
2022-08-20 20:30:50 -0500

    Override classes from md text

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,4 +1,5 @@
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 import PostHeader from './post-header';
 import styles from './post-content.module.css';
 
@@ -6,13 +7,47 @@ const PostContent = (props) => {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customComponent = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       className={styles.image}
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+      if(node.children[0].tagName === 'img') {
+        const image = node.children[0];
+        return (
+          <div className={styles.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>  
+        );
+      }
+
+      return <p>{paragraph.children}</p>
+    }
+  };
+
   return (
     <article className={styles.content}>
       <PostHeader 
         title={post.title} 
         image={imagePath}
       />
-      <ReactMarkdown>
+      <ReactMarkdown
+        components={customComponent}
+      >
         {post.content}
       </ReactMarkdown>
     </article>
