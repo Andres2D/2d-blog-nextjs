65c15f133fb230012cf0307bb0ec1cd14ce272a7
Andres2D <andres2d1997.1206@gmail.com>
2022-08-22 18:55:55 -0500

    Add portals for the notification component

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -16,12 +16,12 @@ function Notification(props) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal((
     <div className={cssClasses}>
       <h2 className={classes.title}>{title}</h2>
       <p className={classes.subtitle}>{message}</p>
     </div>
-  );
+  ), document.getElementById('notifications'));
 }
 
 export default Notification;
