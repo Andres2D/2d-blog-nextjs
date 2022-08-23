4a61936475d36acfb15c968452f30a87f6a5bfb8
Andres2D <andres2d1997.1206@gmail.com>
2022-08-22 19:51:50 -0500

    Add fixed navbar and update styles

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,11 @@
 import MainNav from "./main-nav";
+import styles from './layout.module.css';
 
 const Layout = (props) => {
   return (
     <>
       <MainNav />
-      <main>{props.children}</main>
+      <main className={styles.main}>{props.children}</main>
     </>
   );
 }
--- components/layout/main-nav.js
+++ components/layout/main-nav.js
@@ -1,12 +1,14 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Logo from './logo';
 import styles from './main-nav.module.css';
 
 const MainNav = () => {
+  const { pathname } = useRouter();
   return (
     <header className={styles.header}>
       <Link href='/'>
-        <a className={styles.link}>
+        <a>
           <Logo />
         </a>
       </Link>
@@ -14,12 +16,22 @@ const MainNav = () => {
         <ul className={styles.list}>
           <li className={styles.item}>
             <Link href='/posts'>
-              <a className={styles.link}>Posts</a>
+              <a 
+                className={
+                  `${styles.link}
+                  ${pathname === '/posts' ? styles.activeLink : ''}`
+                }
+              >Posts</a>
             </Link>
           </li>
           <li className={styles.item}>
             <Link href='/contact'>
-              <a className={styles.link}>Contact</a>
+              <a 
+                className={
+                  `${styles.link}
+                  ${pathname === '/contact' ? styles.activeLink : ''}`
+                }
+              >Contact</a>
             </Link>
           </li>
         </ul>
