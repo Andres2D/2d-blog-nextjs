8e3fe72dee2cacc88804b2ccf0e8324ed9580fe8
Andres2D <andres2d1997.1206@gmail.com>
2022-08-23 19:08:30 -0500

    Add personal links in the header

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './hero.module.css';
 
 const Hero = () => {
@@ -14,6 +15,56 @@ const Hero = () => {
       </div>
       <h1 className={styles.title}>HI, I AM ANDRES</h1>
       <p className={styles.subtitle}>I blog about web development - specially frontend with Javascript</p>
+      <section className={styles.links}>
+        <div>
+          <Link href='https://github.com/Andres2D'>
+            <a className={styles.link}>
+              <Image 
+                className={styles.icon}
+                src='/images/site/github-icon.webp'
+                alt='github profile'
+                width={40}
+                height={40}
+              />
+            </a>
+          </Link>
+          <Link href='https://andres-alcaraz.netlify.app/'>
+            <a className={styles.link}>
+              <Image 
+                className={styles.icon}
+                src='/images/site/computer-page.png'
+                alt='personal web page'
+                width={40}
+                height={40}
+              />
+            </a>
+          </Link>
+        </div>
+        <div>
+          <Link href='https://www.facebook.com/andres.alcaraz.794'>
+            <a className={styles.link}>
+              <Image 
+                className={styles.icon}
+                src='/images/site/facebook.svg'
+                alt='Facebook'
+                width={40}
+                height={40}
+              />
+            </a>
+          </Link>
+          <Link href='https://www.instagram.com/andres.2d/'>
+            <a className={styles.link}>
+              <Image 
+                className={styles.icon}
+                src='/images/site/instagram_icon.webp'
+                alt='instagram'
+                width={40}
+                height={40}
+              />
+            </a>
+          </Link>
+        </div>
+      </section>
     </section>
   );
 };
